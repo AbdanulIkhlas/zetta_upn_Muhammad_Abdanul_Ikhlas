68096e1b5ddcfb8ca1053f43da86d65d47f66852
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-03-04 10:47:06 +0700

    feat : added change money

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let buyBook = function (name, price, discount, tax, pay) {
       return false;
     }
   };
+  let changeMoney = pay - totalPrice;
 
   console.log("Name book : " + name);
   console.log("Price book : Rp " + price);
@@ -20,9 +21,12 @@ let buyBook = function (name, price, discount, tax, pay) {
   console.log("Price after adding tax is : Rp " + totalPrice);
   console.log("So the total price to be paid is " + totalPrice);
   console.log("amount of money for payment : " + pay);
-
   if (canBuyBook(pay)) {
     console.log("You can buy this book");
+    if (pay > totalPrice) {
+      console.log("The change : Rp " + changeMoney);
+    }
+    console.log("\nThank you for buying this book");
   } else {
     console.log("You can't buy this book because payment is not enough");
   }
