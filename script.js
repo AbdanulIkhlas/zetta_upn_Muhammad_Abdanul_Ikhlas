8f7d24e6c311c6220ee60a99a9763859cb493629
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-03-15 09:37:00 +0700

    [update] update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let buyBook = function (name, price, discount, tax, pay) {
+let buyBook = function (name, price, discount, tax, pay, stock, amountOfBooks) {
   const theTax = (tax / 100) * price;
   const theDiscount = (discount / 100) * price;
   const totalPrice = price - theDiscount + theTax;
+  const max = stock * 10;
   let canBuyBook = function (pay) {
     if (pay >= totalPrice) {
       return true;
@@ -21,6 +22,21 @@ let buyBook = function (name, price, discount, tax, pay) {
   console.log("the tax : " + tax + "% from the price -> Rp " + theTax);
   console.log("Price after adding tax is : Rp " + totalPrice);
   console.log("So the total price to be paid is " + totalPrice);
+
+  // for (i = 0; i < max; i++) {
+  //   if (stock == 0) {
+  //     console.log("Book stock is running out");
+  //     break;
+  //   }
+  //   if (amountOfBooks <= stock) {
+  //     console.log("existing book stock : " + stock);
+  //     console.log("the number of books you want to buy : " + amountOfBooks);
+  //   } else {
+  //     console.log("insufficient stock of books!!");
+  //     i = max + 1;
+  //   }
+  // }
+
   console.log("amount of money for payment : " + pay);
   if (canBuyBook(pay)) {
     console.log("You can buy this book");
