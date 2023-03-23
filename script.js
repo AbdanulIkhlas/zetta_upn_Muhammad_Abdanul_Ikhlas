5379853499fc98af9150d0fbabfa942efe0fdb7e
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-03-23 11:48:31 +0700

    feat : trying to add rest api with basic authentification

--- script.js
+++ script.js
@@ -1,63 +1,77 @@
-let buyBook = function (
-  name,
-  price,
-  discount,
-  tax,
-  stock,
-  amountOfBooks,
-  termOfCredit
-) {
-  //! calculate and assignt parameter to variabel
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const data = fs.readFileSync("request.json");
+const body = JSON.parse(data);
+
+// Middleware for basic authentication
+const basicAuth = require("express-basic-auth");
+app.use(
+  basicAuth({
+    users: { username: "password" },
+    unauthorizedResponse: { message: "Unauthorized" },
+  })
+);
+
+app.post("/buyBook", (req, res) => {
+  // Retrieve parameters from request body
+  const { name, price, discount, tax, stock, amountOfBooks, termOfCredit } =
+    body;
+
+  // Perform calculations
   const theTax = (tax / 100) * price;
   const theDiscount = (discount / 100) * price;
   let totalPrice = price - theDiscount + theTax;
   let thanks = "Thank you for buying this book";
 
-  //! display
-  console.log("Name book : " + name);
-  console.log("Price book : Rp " + price);
-  console.log(
-    "the discount : " + discount + "% from the price -> Rp " + theDiscount
-  );
-  console.log("Price after discount is : Rp " + (price - theDiscount));
-  console.log("the tax : " + tax + "% from the price -> Rp " + theTax);
-  console.log("Price after adding tax is : Rp " + totalPrice);
-  console.log("Amount of stock : " + stock);
-  console.log("Amount of purchase book : " + amountOfBooks);
-
-  //! Calculate the price based on the number of books purchased
+  // Calculate the price based on the number of books purchased
   let tempTotal = totalPrice;
   totalPrice = 0;
   for (let i = 0; i < amountOfBooks; i++) {
     if (stock <= 0) {
-      console.log("Amount of book is already out of stock!!!");
-      console.log("You can only buy " + i + " books");
-      break;
+      return res.status(400).send({
+        message:
+          "Amount of book is already out of stock!!! You can only buy " +
+          i +
+          " books",
+      });
     } else {
       stock--;
       totalPrice += tempTotal;
     }
   }
 
-  //! initialization montly fee
+  // Initialize monthly fee
   let monthlyFee = totalPrice / termOfCredit;
 
-  //! initialization credit due with array object
+  // Initialize credit due with array object
   let arrayCreditDue = Array.from({ length: termOfCredit }, (i) => ({
     month: i + 1,
     amount: monthlyFee,
   }));
 
-  //! display
-  console.log("The total price to be paid is " + totalPrice);
-  console.log("Monthly payment amount : Rp " + monthlyFee);
-  console.log("\nCredit Due : ");
-  arrayCreditDue.forEach((payment) => {
-    console.log(
-      "Payment amount for month " + payment.month + " is Rp " + payment.amount
-    );
-  });
+  // Prepare response
+  const response = {
+    name: name,
+    price: price,
+    discount: discount,
+    priceAfterDiscount: price - theDiscount,
+    tax: tax,
+    priceAfterTax: totalPrice,
+    stock: stock,
+    amountOfBooks: amountOfBooks,
+    totalPrice: totalPrice,
+    monthlyFee: monthlyFee,
+    creditDue: arrayCreditDue,
+    remainingStock: stock,
+    message: thanks,
+  };
 
-  console.log("Remaining books : " + stock);
-  console.log("\n" + thanks);
-};
+  return res.status(200).send(response);
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
